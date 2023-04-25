b4eabc509a4db3623485139ea5381163c09d7dca
SOWMIYA S <84454998+sowmiyasenguttuvan03@users.noreply.github.com>
2023-04-25 17:52:38 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "A, B and C can do a piece of work in 20, 30 and 60 days respectively. In how many days can A do the work if he is assisted by B and C on every third day?",
+    answer: "15 days",
+    options: [
+      "12 days",
+      "15 days",
+      "16 days",
+      "18 days"
+    ]
+  },
+    {
+    numb: 2,
+    question: "A alone can do a piece of work in 6 days and B alone in 8 days. A and B undertook to do it for Rs. 3200. With the help of C, they completed the work in 3 days. How much is to be paid to C?",
+    answer: "Rs. 400",
+    options: [
+      "Rs. 375",
+      "Rs. 400",
+      "Rs. 600",
+      "Rs. 800"
+    ]
+  },
+    {
+    numb: 3,
+    question: "If 6 men and 8 boys can do a piece of work in 10 days while 26 men and 48 boys can do the same in 2 days, the time taken by 15 men and 20 boys in doing the same type of work will be:",
+    answer: "4 days",
+    options: [
+      "4 days",
+      "5 days",
+      "6 days",
+      "7 days"
+    ]
+  },
+    {
+    numb: 4,
+    question: "A can do a piece of work in 4 hours; B and C together can do it in 3 hours, while A and C together can do it in 2 hours. How long will B alone take to do it?",
+    answer: "12 hours",
+    options: [
+      "8 hours",
+      "10 hours",
+      "12 hours",
+      "24 hours"
+    ]
+  },
+    {
+    numb: 5,
+    question: "A can do a certain work in the same time in which B and C together can do it. If A and B together could do it in 10 days and C alone in 50 days, then B alone could do it in:",
+    answer: "25 days",
+    options: [
+      "15 days",
+      "20 days",
+      "25 days",
+      "30 days"
+    ]
+  }
+];
